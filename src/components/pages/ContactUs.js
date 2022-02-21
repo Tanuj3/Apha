fda7aa7d546b800aef275eddfa31e962fb50f6c1
unknown <hitenbafna2201@gmail.com>
2022-02-21 13:45:33 +0530

    message

--- src/components/pages/ContactUs.js
+++ src/components/pages/ContactUs.js
@@ -2,5 +2,9 @@ import React from 'react';
 import '../../App.css';
 
 export default function ContactUs() {
-  return <h1 className='contact-us'>CONTACT HANDLES</h1>;
+  return (
+  <>
+  <h1 className='contact-us'>CONTACT HANDLES</h1>;
+  </>
+  );
 }
