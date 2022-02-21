72327906df006228364dee01270157aebd73d014
unknown <hitenbafna2201@gmail.com>
2022-02-21 14:15:17 +0530

    message

--- src/components/pages/ContactUs.js
+++ src/components/pages/ContactUs.js
@@ -19,25 +19,25 @@ export default function ContactUs() {
         <div class="address details">
           <i class="fas fa-map-marker-alt"></i>
           <div class="topic">Address</div>
-          <div class="text-one">Surkhet, NP12</div>
-          <div class="text-two">Birendranagar 06</div>
+          <div class="text-one">South Delhi, New Delhi</div>
+          <div class="text-two">India</div>
         </div>
         <div class="phone details">
           <i class="fas fa-phone-alt"></i>
           <div class="topic">Phone</div>
-          <div class="text-one">+0098 9893 5647</div>
-          <div class="text-two">+0096 3434 5678</div>
+          <div class="text-one">+918588958674</div>
+          <div class="text-two">+919560055140</div>
         </div>
         <div class="email details">
           <i class="fas fa-envelope"></i>
           <div class="topic">Email</div>
-          <div class="text-one">codinglab@gmail.com</div>
-          <div class="text-two">info.codinglab@gmail.com</div>
+          <div class="text-one">info@thekaurresearch.com</div>
+          <div class="text-two">teji@thekaurresearch.com</div>
         </div>
       </div>
       <div class="right-side">
         <div class="topic-text">Send us a message</div>
-        <p>If you have any work from me or any types of quries related to my tutorial, you can send me message from here. It's my pleasure to help you.</p>
+        <p>If you have types of quries related to us, you can send us a message from here. It's our pleasure to help you.</p>
       <form action="#">
         <div class="input-box">
           <input type="text" placeholder="Enter your name"/>
@@ -46,7 +46,7 @@ export default function ContactUs() {
           <input type="text" placeholder="Enter your email"/>
         </div>
         <div class="input-box message-box">
-          
+          <input type= "text" placeholder="Enter your message"/>
         </div>
         <div class="button">
           <input type="button" value="Send Now" />
